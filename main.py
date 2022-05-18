a60064799aedacd817a724ee22078ee5a7441776
MeitalDukarker <97167029+MeitalDukarker@users.noreply.github.com>
2022-05-18 15:11:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-import pygame, sys
+import pygame, sys, random
+#from pygame.constants import K_DOWN
+import cv2
+import mediapipe as mp
 
-from pygame.constants import K_DOWN
 
 def ball_animation ():
-    global ball_speed_x , ball_speed_y
+    global ball_speed_x , ball_speed_y, player_score, opponent_score
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top<=0 or ball.bottom>=window_h:
+        pygame.mixer.Sound.play(pong_sound)
         ball_speed_y *=-1
-    if ball.left<=0 or ball.right>=window_w:
-        ball_speed_x *= -1
-
+    if ball.left<=0:
+        opponent_score += 1
+        pygame.mixer.Sound.play(score_sound)
+        ball_restart()
+    if ball.right>=window_w:
+        pygame.mixer.Sound.play(score_sound)
+        player_score += 1
+        ball_restart()
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x*= -1
 
 def player_animation():
+    global player_speed
     player.y += player_speed
+    player_speed = 0
     if player.top<=0:
         player.top=0
     if player.bottom>= window_h:
@@ -32,6 +42,20 @@ def opponent_ai():
     if opponent.bottom>= window_h:
         opponent.bottom= window_h
 
+def ball_restart():
+    global ball_speed_x , ball_speed_y
+    ball.center= (window_w/2, window_h/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
+
+
+pygame.init()
+clock= pygame.time.Clock()
+
+player_score=0
+opponent_score=0
+game_font= pygame.font.SysFont(None, 32)
+
 
 
 #screen size
@@ -39,8 +63,7 @@ window_w= 900
 window_h=500
 window_size=(window_w, window_h)
 
-clock= pygame.time.Clock()
-pygame.init()
+
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Pong")
 
@@ -51,25 +74,91 @@ opponent= pygame.Rect(10,window_h/2-70,10,140)
 bg_color=pygame.Color('grey12')
 light_grey=(200,200,200)
 
-ball_speed_x=7
-ball_speed_y=7
+ball_speed_x=7 * random.choice((1,-1))
+ball_speed_y=7 * random.choice((1,-1))
 player_speed=0
 opponent_speed= 7
+
+pong_sound= pygame.mixer.Sound("pong.ogg")
+score_sound= pygame.mixer.Sound("score.ogg")
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+mp_Hands = mp.solutions.hands
+hands = mp_Hands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
+    # Capture the video frame
+	# by frame
+    ret, frame = vid.read()
+    index_finger_x = 0
+
+    RGB_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	
+    results = hands.process(RGB_image)
+    multiLandMarks = results.multi_hand_landmarks
+    
+    if multiLandMarks:
+        # go over all hands found and draw them on the BGR image
+        for handLms in multiLandMarks:
+            mpDraw.draw_landmarks(frame, handLms, mp_Hands.HAND_CONNECTIONS)
+            # the location of the index finger tip is number 8 in the landmarks
+        index_finger_y = multiLandMarks[0].landmark[8].y
+        index_finger_y1=multiLandMarks[0].landmark[5].y
+    # Display the resulting frame
+    # font
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    # org
+    org = (50, 50)
+    
+    # fontScale
+    fontScale = 1
+    
+    # Blue color in BGR
+    color = (255, 0, 0)
+    
+    # Line thickness of 2 px
+    thickness = 2
+    
+    # Using cv2.putText() method
+    frame = cv2.putText(frame, str(index_finger_y), org, font, 
+                    fontScale, color, thickness, cv2.LINE_AA)
+    cv2.imshow('frame', frame)
+    
+    if index_finger_y > index_finger_y1:
+        print("down")
+    else:
+        print("up")
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit() 
             sys.exit()
         if event.type==pygame.KEYDOWN:
             if event.key== pygame.K_DOWN:
-                player_speed +=7
+                player_speed +=20
             if event.key== pygame.K_UP:
-                player_speed -=7
+                player_speed -=20
         if event.type==pygame.KEYDOWN:
             if event.key== pygame.K_UP:
-                player_speed -=7
+                player_speed -=20
             if event.key== pygame.K_DOWN:
-                player_speed +=7
+                player_speed +=20
+                
+        if index_finger_y>0.7:
+            player_speed-=20    
+        if index_finger_y<0.4:
+            player_speed+=20
 
     ball_animation() 
     player_animation()
@@ -82,5 +171,18 @@ while True:
     pygame.draw.ellipse(screen, light_grey,ball)
     pygame.draw.aaline(screen, light_grey,(window_w/2,0),(window_w/2,window_h))
 
+
+    player_text= game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (430,240))
+
+    opponent_text= game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (460,240))
+
+
     pygame.display.flip()
-    clock.tick(50)
+    clock.tick(100)
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
